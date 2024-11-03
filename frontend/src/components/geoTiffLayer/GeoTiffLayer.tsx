cb4faa3720f10df437ae60a38aa0a808ef4e24ea
Maxime <32384488+maxime-mrl@users.noreply.github.com>
2024-11-03 10:38:50 +0100

    fixed geotifflayer

--- frontend/src/components/geoTiffLayer/GeoTiffLayer.tsx
+++ frontend/src/components/geoTiffLayer/GeoTiffLayer.tsx
@@ -10,39 +10,26 @@ import "leaflet-geotiff-2/dist/leaflet-geotiff-plotty"; // requires plotty
 
 
 export default function GeoTiffLayer({ url }: {url: string}) {
-    const map = useMap();
-    useEffect(() => {
-      const geoTiffLayer = new L.LeafletGeotiff(url, {
-        band: 0,
-        renderer: L.LeafletGeotiff.plotty({
-          displayMin: 0,
-          displayMax: 255,
-          colorScale: "rainbow",
-        }),
-      });
-      // Add the layer to the map
-      geoTiffLayer.addTo(map);
-      // Cleanup
-      return () => { map.removeLayer(geoTiffLayer) };
-    }, [map, url]);
-  
-    return null; // No visual component to render
-  };
+  const map = useMap();
+  useEffect(() => {
+    let isError = false;
+    const geoTiffLayer = new L.LeafletGeotiff(url, {
+      band: 0,
+      renderer: L.LeafletGeotiff.plotty({
+        displayMin: 0,
+        displayMax: 255,
+        colorScale: "rainbow",
+      }),
+      onError: (er:any) => {
+        isError = true;
+        console.log(`error: ${er}`)
+      }, 
+    });
+    // Add the layer to the map
+    if (!isError) geoTiffLayer.addTo(map);
+    // Cleanup
+    return () => { map.removeLayer(geoTiffLayer) };
+  }, [map, url]);
 
-// export default function GeoTiffLayer2({ url }) {
-//   const map = useMap();
-
-//   useEffect(() => {
-//     const bounds = L.latLng([37, -78]).toBounds(200000)
-//     const square = new L.Rectangle(bounds)
-//     // const container = context.layerContainer || context.map
-//     map.addLayer(square)
-//     // container.addLayer(square)
-
-//     return () => {
-//       map.removeLayer(square)
-//     }
-//   })
-
-//   return null
-// }
+  return null; // No visual component to render
+};
--- frontend/types/leaflet-geotiff.d.ts
+++ frontend/types/leaflet-geotiff.d.ts
@@ -7,27 +7,49 @@ declare module "leaflet" {
     // Static renderer methods
     static plotty(options: PlottyOptions): LeafletGeotiffRenderer;
     static vectorArrows(options: VectorArrowsOptions): LeafletGeotiffRenderer;
+    static rgb(options: RGBOptions): LeafletGeotiffRenderer;
   }
 
   interface LeafletGeotiffOptions extends GridLayerOptions {
+    renderer: LeafletGeotiffRenderer;
+    useWorker?: boolean;
+    bounds?: [LatLng, LatLng];
     band?: number;
-    renderer?: LeafletGeotiffRenderer;
-    clampLow?: boolean;
-    clampHigh?: boolean;
-    noDataValue?: number;
-    name?: string;
+    image?: number;
+    clip?: LatLng[];
+    pane?: any; // din't understand what is this / i dont need it
+    onError?: CallableFunction;
+    sourceFunction?: GeoTIFF.fromArrayBuffer | GeoTIFF.fromUrl | GeoTIFF.fromBlob;
+    arrayBuffer?: ArrayBuffer;
+    noDataValue?: number; 
+    noDataKey?: any; // also not sure 
+    blockSize?: number;
+    opacity?: number;
+    clearBeforeMove?: boolean;
+
     colorScale?: string;
   }
 
   interface PlottyOptions {
-    colorScale?: string;
     displayMin?: number;
     displayMax?: number;
+    applyDisplayRange?: boolean;
+    clampLow?: boolean;
+    clampHigh?: boolean;
+    colorScale?: string;
   }
 
   interface VectorArrowsOptions {
     arrowSize?: number;
-    color?: string;
+  }
+
+  interface RGBOptions {
+    rBand?: number,
+    gBand?: number,
+    bBand?: number,
+    alphaBand?: number,
+    transpValue?: number,
+    renderer: any, // wtf?
   }
 
   interface LeafletGeotiffRenderer {}
